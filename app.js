6b609ea2dc979ee3729fb919dccb8e52ec42e789
root <mwf415@126.com>
2018-10-14 16:13:56 +0800

    first submit

--- app.js
+++ app.js
@@ -41,27 +41,27 @@ app.use(session({//session持久化配置
 }));
 
 //ueditor上传图片
-app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function (req, res, next) {
-  // ueditor 客户发起上传图片请求
-  if (req.query.action === 'uploadimage') {
-    var foo = req.ueditor;
-    var imgname = req.ueditor.filename;
-    var img_url = '/images/ueditor/';
-    res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
-    res.setHeader('Content-Type', 'text/html');//IE8下载需要设置返回头尾text/html 不然json返回文件会被直接下载打开
-  }
-  //  客户端发起图片列表请求
-  else if (req.query.action === 'listimage') {
-    var dir_url = '/images/ueditor/';
-    res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片
-  }
-  // 客户端发起其它请求
-  else {
-    // console.log('config.json')
-    res.setHeader('Content-Type', 'application/json');
-    res.redirect('/ueditor/jsp/config.json');
-  }
-}));
+// app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function (req, res, next) {
+//   // ueditor 客户发起上传图片请求
+//   if (req.query.action === 'uploadimage') {
+//     var foo = req.ueditor;
+//     var imgname = req.ueditor.filename;
+//     var img_url = '/images/ueditor/';
+//     res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+//     res.setHeader('Content-Type', 'text/html');//IE8下载需要设置返回头尾text/html 不然json返回文件会被直接下载打开
+//   }
+//   //  客户端发起图片列表请求
+//   else if (req.query.action === 'listimage') {
+//     var dir_url = '/images/ueditor/';
+//     res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片
+//   }
+//   // 客户端发起其它请求
+//   else {
+//     // console.log('config.json')
+//     res.setHeader('Content-Type', 'application/json');
+//     res.redirect('/ueditor/jsp/config.json');
+//   }
+// }));
 //ueditor上传图片
 
 app.get('/captcha', function (req, res) {
